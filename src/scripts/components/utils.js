41ae13a2d17e2c4b51e0e6c6c1585a29f2b591e8
ShockIgorek <shokin1996@yandex.ru>
2022-02-06 07:24:24 +0700

    fix git

--- src/scripts/components/utils.js
+++ src/scripts/components/utils.js
@@ -9,56 +9,9 @@ const config = {
 
 };
 
-function pathLikes(id) {
-  fetch(config.baseUrl + '/cards/likes/' + id, {
-    method: 'PUT',
-    headers: config.headers,
-  })
 
-}
-function deleteLikes(id) {
-  fetch(config.baseUrl + '/cards/likes/' + id, {
-    method: 'DELETE',
-    headers: config.headers,
-  })
-}
 
-function updateLikes(id) {
-  fetch(config.baseUrl + '/cards/likes/' + id, {
-      headers: config.headers
-    })
-    .then(res => res.json())
-    .then((result) => {
-      console.log(result)
-    })
 
-}
-function postCard(card, link) {
-  const popup = document.querySelector('.popup_add-photo')
-  fetch(config.baseUrl + '/cards', {
-      method: 'POST',
-      headers: config.headers,
-      body: JSON.stringify({
-        name: card,
-        link: link,
-      })
-    }).then(res => res.json())
-    .then((result) => {
-      console.log(result._id)
-      closePopup(popup)
-      return result
-    }).catch((err => {
-      console.log(err);
-      closePopup(popup)
-    }))
-}
-
-function deleteCard(id) {
-  fetch(config.baseUrl + '/cards/' + id, {
-    method: 'DELETE',
-    headers: config.headers,
-  })
-}
 
 function getName() {
   fetch(config.baseUrl + '/users/me', {
@@ -70,46 +23,6 @@ function getName() {
       profileDescription.textContent = result.about;
     })
 }
-
-function checkMyLike(result ,index) {
-  for (let i = 0; i < Object(result[index].likes).length; i++) {
-    if (Object(result[index].likes[i]).name === getName()) {
-      return true;
-    }
-  }
-}
-function checkMyCard(result, index) {
-  if (Object(result[index].owner).name === getName()) {
-    return true;
-  }
-}
-
-const getCards= () => {
-  return fetch(config.baseUrl + '/cards', {
-      headers: config.headers
-    }).then(res => getResponseData(res))
-    .then(res => res.json())
-    .then((result) => {
-      for (let i = 0; i < result.length; i++) {
-        cards[i] = {
-          name: result[i].name,
-          link: result[i].link,
-          likes: result[i].likes.length,
-          id: result[i]._id,
-          myLikes: checkMyLike(result, i),
-          myCard: checkMyCard(result, i),
-        }
-      }
-      return (cards)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-//
-
-
-
 
 function pathNewName() {
   fetch(config.baseUrl + '/users/me', {
